6307939aaea87a3cbbc3fdfbd3170f95c309d0aa
prashantbhoite123 <pbhoite985@gmail.com>
2024-09-02 19:47:47 +0530

    complete search api

---
+++ src/controller/RestaurantController.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from "express"
+import { Restaurant } from "../model/restaurant"
+
+const searchRestaurants = async (req: Request, res: Response) => {
+  try {
+    const city = req.params.city
+
+    const searchQuery = (req.query.searchQuery as string) || ""
+    const selectedCuisines = (req.query.selectedCuisines as string) || ""
+    const shortOption = (req.query.shortOption as string) || ""
+    const page = parseInt(req.query.page as string) || 1
+
+    let query: any = {}
+
+    query["city"] = new RegExp(city, "i")
+    const cityCheck = await Restaurant.countDocuments(query)
+
+    if (cityCheck === 0) {
+      return res.status(404).json([])
+    }
+
+    if (selectedCuisines) {
+      const cuisinesArray = selectedCuisines
+        .split(",")
+        .map((cuisine) => new RegExp(cuisine, "i"))
+
+      query["cuisines"] = { $all: cuisinesArray }
+    }
+
+    if (searchQuery) {
+      const searchRegex = new RegExp(searchQuery, "i")
+      query["$or"] = [
+        { restaurantName: searchRegex },
+        { cuisines: { $in: [searchRegex] } },
+      ]
+    }
+
+    const pageSize = 10
+    const skip = (page - 1) * pageSize
+
+    // sortOption = "lastUpdated"
+
+    const restaunrants = await Restaurant.find(query)
+      .sort({ [shortOption]: 1 })
+      .skip(skip)
+      .limit(pageSize)
+      .lean()
+
+    const total = await Restaurant.countDocuments(query)
+
+    const response = {
+      data: restaunrants,
+      pagination: {
+        total,
+        page,
+        pages: Math.ceil(total / pageSize),
+      },
+    }
+
+    res.json(response)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: "Something went wrong" })
+  }
+}
+
+export default {
+  searchRestaurants,
+}
---
+++ src/routes/RestaurantRoute.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import { param } from "express-validator"
+import RestaurantController from "../controller/RestaurantController"
+
+const router = express.Router()
+
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City parameter must be a valid string"),
+  RestaurantController.searchRestaurants
+)
