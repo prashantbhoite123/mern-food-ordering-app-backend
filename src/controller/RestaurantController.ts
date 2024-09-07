876e0c7ea6f55021b1dac1066bf640d5f7417a23
prashantbhoite123 <pbhoite985@gmail.com>
2024-09-08 04:14:59 +0530

    complete search filter functionality

--- src/controller/RestaurantController.ts
+++ src/controller/RestaurantController.ts
@@ -16,7 +16,14 @@ const searchRestaurants = async (req: Request, res: Response) => {
     const cityCheck = await Restaurant.countDocuments(query)
 
     if (cityCheck === 0) {
-      return res.status(404).json([])
+      return res.status(404).json({
+        data: [],
+        pagination: {
+          total: 0,
+          page: 1,
+          pages: 1,
+        },
+      })
     }
 
     if (selectedCuisines) {
